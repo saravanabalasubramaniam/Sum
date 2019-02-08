fa8a3ae8a0d5358822fa1271091ff86224317cad
saravana <saravanasaru14313@gmail.com>
2019-02-08 14:44:26 +0530

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Sum
+{
+ public static void main(String args[])
+ {
+	Scanner input=new Scanner(System.in);
+	int a=input.nextInt();
+	int b=input.nextInt();
+	int c=a+b;
+	System.out.println(c);
+ }
+}
